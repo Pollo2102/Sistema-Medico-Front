d3642d6f2a82bdf441f9ef176b2b2a6f7df95b0a
Diego Valenzuela <valenv.dg@gmail.com>
2021-01-27 23:16:02 -0600

    Registering and showing patients completed.

--- Sistema-Medico-Front/src/app/home/home.component.ts
+++ Sistema-Medico-Front/src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Patient } from '../models/patient'
 import { User_Patient } from '../models/user_patient'
 import { User } from '../models/user';
-import { UserService } from '../services/patient.service'
+import { PatientService } from '../services/patient.service'
 import { UserPatientService } from '../services/user_patient.service';
 
 @Component({
@@ -17,6 +17,20 @@ export class HomeComponent implements OnInit {
   userID: number;
   currentPatient: Patient;
   currUserPatient: User_Patient;
+  getPatientObj: Patient;
+
+// ----------------------------
+// Patient show information: 
+
+  showSexo: string;
+  showEstadoCivil: string;
+  showReligion: string;
+  showEscolaridad: string;
+  showDate: string;
+
+// ----------------------------
+
+  getPatientID: number;
   http: HttpClient;
 
   picker: string;
@@ -25,11 +39,12 @@ export class HomeComponent implements OnInit {
   maxDate: Date;
   
 
-  constructor(private userService: UserService, private userPatientService: UserPatientService) {
+  constructor(private patientService: PatientService, private userPatientService: UserPatientService) {
 
     this.userID = JSON.parse(localStorage.getItem('currentUser')).id;
 
     this.currentPatient = new Patient();
+    this.getPatientObj = new Patient();
     this.currUserPatient = new User_Patient();
 
     const currentYear = new Date().getFullYear();
@@ -55,7 +70,7 @@ export class HomeComponent implements OnInit {
       console.log(JSON.stringify(this.currentPatient));
       if (this.verifyFields()) {
         console.log("Registering User...");
-        this.userService.registerUser(this.currentPatient)
+        this.patientService.registerPatient(this.currentPatient)
         .subscribe(data => {
           console.log(data);
           console.log("User Registered...");
@@ -72,6 +87,73 @@ export class HomeComponent implements OnInit {
       }
       else
         console.log("Failed");
+   }
+
+   getPatient = () => {
+      if (!this.getPatientID) {
+        console.log(`Patient id empty: ${this.getPatientID}`);
+        return;
+      }
+      this.patientService.getPatient(this.getPatientID)
+      .subscribe(data => {
+        this.getPatientObj = data;
+        this.updateGetFields();
+      })
+   }
+
+   updateGetFields = () => {
+
+      if (this.getPatientObj.sexo == 1)
+        this.showSexo = "Masculino";
+      else if (this.getPatientObj.sexo == 2)
+        this.showSexo = "Femenino";
+
+      if (this.getPatientObj.estado_civil == 0)
+        this.showEstadoCivil = "Soltero"
+      else if (this.getPatientObj.estado_civil == 1)
+        this.showEstadoCivil = "Casado";
+
+      if (this.getPatientObj.religion == 0)
+        this.showReligion = 'Católico';
+      else if (this.getPatientObj.religion == 1)
+        this.showReligion = 'Ortodoxo';
+      else if (this.getPatientObj.religion == 2)
+        this.showReligion = 'Protestante';
+      else if (this.getPatientObj.religion == 3)
+        this.showReligion = 'Musulman';
+      else if (this.getPatientObj.religion == 4)
+        this.showReligion = 'Budista';
+      else if (this.getPatientObj.religion == 5)
+        this.showReligion = 'Judío';
+      else if (this.getPatientObj.religion == 6)
+        this.showReligion = 'Hindú';
+      else if (this.getPatientObj.religion == 7)
+        this.showReligion = 'Taoista';
+      else if (this.getPatientObj.religion == 8)
+        this.showReligion = 'Ateo';
+      else if (this.getPatientObj.religion == 9)
+        this.showReligion = 'Otro';
+
+      if (this.getPatientObj.escolaridad == 0)
+        this.showEscolaridad = 'Desconocido';
+      if (this.getPatientObj.escolaridad == 1)
+        this.showEscolaridad = 'Primaria';
+      if (this.getPatientObj.escolaridad == 2)
+        this.showEscolaridad = 'Secundaria';
+      if (this.getPatientObj.escolaridad == 3)
+        this.showEscolaridad = 'Universidad';
+
+        let newDate = new Date(this.getPatientObj.fecha_nacimiento);
+
+        let month = String(newDate.getMonth() + 1);
+        let day = String(newDate.getDate());
+        const year = String(newDate.getFullYear());
+      
+        if (month.length < 2) month = '0' + month;
+        if (day.length < 2) day = '0' + day;
+      
+        this.showDate =  `${month}/${day}/${year}`;
+
    }
 
   ngOnInit(): void {
--- Sistema-Medico-Front/src/app/services/patient.service.ts
+++ Sistema-Medico-Front/src/app/services/patient.service.ts
@@ -5,10 +5,15 @@ import { environment } from '../../environments/environment';
 import { Patient } from '../models/patient';
 
 @Injectable({ providedIn: 'root' })
-export class UserService {
+export class PatientService {
     constructor(private http: HttpClient) { }
 
-    registerUser(patient: Patient) {
+    registerPatient = (patient: Patient) => {
         return this.http.post<any>(`${environment.apiUrl}/patients/insert`, patient);
     }
+
+    getPatient = (patientID: number) => {
+        return this.http.get<Patient>(`${environment.apiUrl}/patients/get/${patientID}`)
+    }
+
 }
