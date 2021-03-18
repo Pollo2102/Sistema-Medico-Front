dd83f9ca164e9d9dd484e2daf60226a58776ad11
Diego Valenzuela <valenv.dg@gmail.com>
2021-03-17 21:12:18 -0600

    Project ready for production. Deployed using firebase cli.

--- Sistema-Medico-Front/src/main.ts
+++ Sistema-Medico-Front/src/main.ts
@@ -4,6 +4,26 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBlYrS7i04GtqD-OGlUqiFDtfhn8_-yH6M",
+  authDomain: "sis-med-front-cbd8e.firebaseapp.com",
+  projectId: "sis-med-front-cbd8e",
+  storageBucket: "sis-med-front-cbd8e.appspot.com",
+  messagingSenderId: "680678637294",
+  appId: "1:680678637294:web:719cb7f6b9a2d7e6a5bdff",
+  measurementId: "G-3Q8PZSDDZQ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
 if (environment.production) {
   enableProdMode();
 }
